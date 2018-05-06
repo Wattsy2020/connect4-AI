b4df465cd00676a4ccd317e10462618ebba6ec26
Liam Watts <35185935+Wattsy2020@users.noreply.github.com>
2018-05-06 13:11:23 +1000

    Updates to gameTree class
    -genNewLevel() and gameTree() methods have been implemented in the gameTree class
    -removed the old analysePosition() method as it is redundant
    -various other minor improvements

--- connect4ai.java
+++ connect4ai.java
@@ -26,121 +26,16 @@ class Analysis{
         return 2;
     }
     
-    //Determines whether a positions is won, lost or even
-    public static int analysePosition(Position pos){
-        
-        //check rows
-        int fullRows = 6;
-        for (int i = 0; i < 6; i++){
-            //check if row is empty
-            int sum = 0;
-            for (int j = 0; j < 7; j++){
-                sum += pos.board[i][j];
-            }           
-            if (sum == 14){ //i.e. there are 7 twos so the row is empty
-                fullRows -= 1;
-            }
-            
-            int result = connected(pos.board[i]);
-            if (result != 2){
-                return result;
-            }
-        }
-        
-        if (fullRows < 4){ //can't have 4 in a row in a column/diagonal if less than 4 rows
-            return 2;
-        }
-        
-        //check columns
-        int[] column = new int[fullRows];
-        for (int j = 0; j < 7; j++){
-            //construct the column array from bottom row to top
-            for (int k = 5; k > 5 - fullRows; k--){
-                column[5 - k] = pos.board[k][j];
-            }
-            
-            int result = connected(column);
-            if (result != 2){
-                return result;
-            }
-        }
-        
-        //check diagonals from [0,0] to [6,6] then [1,0] to [6,5] then [2,0] to [6,4]
-        for (int m = 0; m < 3; m++){
-            //construct diagonal array
-            int[] diagonal = new int [6 - m];
-            int i = 0;
-            for (int n = 6 - fullRows; n < 6; n++){
-                if ((n - m) < 0){
-                    continue;
-                }
-                diagonal[i] = pos.board[n][n-m];
-                i++;
-            }
-            
-            int result = connected(diagonal);
-            if (result != 2){
-                return result;
-            }           
-        }
-        
-        //check diagonals from [0, 1] to [6,7] than [0,2] to [5,7] then [0,3] to [4,7]
-        for (int q = 1; q < 4; q++){
-            int[] diagonal = new int[1 + fullRows - q];
-            for (int r = 6 - fullRows; r < (7 - q); r++){
-                diagonal[r + fullRows - 6] = pos.board[r][q + r];
-            }
-            
-            int result = connected(diagonal);
-            if (result != 2){
-                return result;
-            }
-        }
-        
-        //check diagonals from [0, 7] to [6,1] than [1, 7] to [6,2] ...
-        for (int o = 0; o < 3; o++){
-            int[] diagonal = new int [6 - o];
-            int i = 0;
-            for (int p = 6 - fullRows; p < 6; p++){
-                if ((p - o) < 0){
-                    continue;
-                }
-                diagonal[i] = pos.board[p][6 - (p - o)];
-                i++;
-            }
-            
-            int result = connected(diagonal);
-            if (result != 2){
-                return result;
-            }           
-        }
-        
-        //check diagonals from [0, 6] to [6,0] than [0,5] to [5,0] then [0,3] to [4,7]
-        for (int s = 1; s < 4; s++){
-            int[] diagonal = new int[1 + fullRows - s];
-            for (int t = 6 - fullRows; t < (7 - s); t++){
-                diagonal[t + fullRows - 6] = pos.board[t][6 - (s + t)];
-            }
-            
-            int result = connected(diagonal);
-            if (result != 2){
-                return result;
-            }
-        }
-        
-        return 2;
-    }
-    
-    //Analyse a position created from a parent (only have to check the modified row, column and diagonals)
-    public static int analyseChildPosition(Position child, int[] move){
+    //Analyse a position created from an even parent (only have to check the modified row, column and diagonals)
+    public static int analysePosition(int[][] board, int[] move){
         //check row
-        int result = connected(child.board[move[0]]);
+        int result = connected(board[move[0]]);
         if (result != 2){ return result;}
         
         //check column
         int[] column = new int[6];
         for(int i = 0; i < 6; i++){
-            column[i] = child.board[i][move[1]];
+            column[i] = board[i][move[1]];
         }
         result = connected(column);
         if (result != 2){ return result;}
@@ -163,7 +58,7 @@ class Analysis{
         int[] diagonal = new int[length];
         int i = 0;
         while (i < length){
-            diagonal[i] = child.board[topLeftStart[0] + i][topLeftStart[1] + i];
+            diagonal[i] = board[topLeftStart[0] + i][topLeftStart[1] + i];
             i++;
         }
         
@@ -187,7 +82,7 @@ class Analysis{
         i = 0;
         int[] diagonal2 = new int[length];
         while (i < length){
-            diagonal2[i] = child.board[topRightStart[0] + i][topRightStart[1] - i];
+            diagonal2[i] = board[topRightStart[0] + i][topRightStart[1] - i];
             i++;
         }
         return connected(diagonal2);
@@ -198,22 +93,21 @@ class Position{
     //the players colour, if analysePosition returns this the position is lost
     public static int lossColour;
     //whether the position is won or lost: -1 = loss, 0 = even, 1 = won
-    public int State;
+    public int state;
     //represents the board as a multidimensional array, 0 = red checker 1 = blue checker 2 = empty
     public int[][] board = new int[6][7];
     //references to parent and children positions, useful for the minimax algorithm
     public Position parent;
     public ArrayList<Position> children = new ArrayList<>();
     
-    //create a position from a multidimensional array
-    public Position(int[][] array){
+    //explicitly define a position (only used for startingPosition or sample positions)
+    public Position(int[][] array, int result){
         for(int i = 0; i < 6; i++){
             System.arraycopy(array[i], 0, board[i], 0, array[i].length);
         }
-        int result = Analysis.analysePosition(this);
-        if (result == 2){State = 0;}
-        else if (result == lossColour){State = -1;}
-        else{State = 1;}
+        if (result == 2){state = 0;}
+        else if (result == lossColour){state = -1;}
+        else{state = 1;}
     }
     
     //create a position by placing a checker in the parent position
@@ -224,10 +118,10 @@ class Position{
         }
         board[move[0]][move[1]] = colour;
         
-        int result = Analysis.analyseChildPosition(this, move);
-        if (result == 2){State = 0;}
-        else if (result == lossColour){State = -1;}
-        else{State = 1;}
+        int result = Analysis.analysePosition(board, move);
+        if (result == 2){state = 0;}
+        else if (result == lossColour){state = -1;}
+        else{state = 1;}
     }
     
     public void displayPosition(){
@@ -256,7 +150,7 @@ class gameTree{
     private int[] rootNode = {0,0};
     
     //Generates the positions possible in the next move from the input position
-    public ArrayList<Position> genNextPositions(Position pos, int colour){
+    private ArrayList<Position> genNextPositions(Position pos, int colour){
         ArrayList<Position> nextPositions = new ArrayList<>();
         for(int i = 0; i < 7; i++){
             if (pos.board[0][i] != 2){continue;} //don't add a checker in a full column
@@ -267,7 +161,7 @@ class gameTree{
                 lowestEmptySpace--;
             }
             
-            //Create new position and append to return positions and the parent's children
+            //Create new position and append to nextPositions and the parent's children
             int[] move = {lowestEmptySpace, i};
             Position newPos = new Position(pos, move, colour);
             nextPositions.add(newPos);
@@ -277,8 +171,39 @@ class gameTree{
     }
     
     //Adds a new level to the tree by calling genNextPositions() on the current level
-    public void genNewLevel(){
+    private void genNewLevel(int colour){
+        ArrayList<Position> newLevel = new ArrayList<>();
+        int Levels = positions.size() - 1;
+        int prevLevelSize = positions.get(Levels).size();
         
+        for(int i = 0; i < prevLevelSize; i++){
+            Position parent = positions.get(Levels).get(i);
+            
+            if (parent.state == 0){ //if the position isn't won or lost
+                ArrayList<Position> children = genNextPositions(parent, colour);
+                
+                //add children positions to newLevel
+                for(int j = 0; j < children.size(); j++){
+                    newLevel.add(children.get(j));
+                }
+            }
+        }
+        positions.add(newLevel);
+    }
+    
+    //initialises the gameTree by repeatedly calling genNewLevel()
+    public gameTree(int depth, Position startPosition, int colour){
+        //define the first level
+        ArrayList<Position> startLevel = new ArrayList<>();
+        startLevel.add(startPosition);
+        positions.add(startLevel);
+        
+        //call genNewLevel() until depth is reached
+        for(int i = 0; i < depth; i++) {
+            genNewLevel(colour);
+            if (colour == 1){colour = 0;} //update the colour
+            else {colour = 1;}
+        }
     }
     
 }
@@ -294,16 +219,15 @@ class Connect4ai {
                                  {2,2,2,2,2,2,2}};
               
         int[][] samplePosArray = {{1,2,2,2,2,2,2},
-                                  {0,2,2,2,2,2,2},
+                                  {0,2,2,2,2,2,1},
                                   {1,1,2,2,2,1,1},
                                   {0,0,1,2,1,0,1},
                                   {0,1,1,0,0,1,0},
                                   {0,1,0,0,0,1,1}};
                 
-        Position startPos = new Position(startPosArray);
-        Position samplePos = new Position(samplePosArray);
-        gameTree tree = new gameTree();
-        
+        Position startPos = new Position(startPosArray, 2);
+        Position samplePos = new Position(samplePosArray, 2);
+        gameTree tree = new gameTree(7, startPos, 1);
         
     }
 }
